8815105212d78935bbce83dd8e3ea97076b42e17
Tao Long <iamtaolong@gmail.com>
2019-09-26 14:22:46 -0400

    Testing the Commit and Push Functions On My New Laptop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+using namespace std;
+
+int arr(int a[], int n) {
+
+    int currentMax = a[0];
+    for (int i = 0; i < n; i++) {
+        if (currentMax < a[i]) {
+            currentMax = a[i];
+        }
+    }
+    return currentMax;
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int *A;
+    int n;
+
+    cout << "Enter the size: " << endl;
+    cin >> n;
+
+    A = new int[n];
+
+    cout << "Enter the each elements in your array" << endl;
+    for (int i; i < n; i++) {
+        cin >> A[i];
+    }
+
+
+    cout << "largest element is : " << arr(A,n) << endl;
+
+    delete A;
     return 0;
+
 }
+
